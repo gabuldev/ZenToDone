9ca256dc27e9ac0e1c55f19e384a4cec54091726
Gabriel Savio <gbrlsavio2@gmailcom>
2017-11-26 19:03:34 -0200

    version 1.0.0.8 tres tabs e to fazendo a parte de editar o card

---
+++ app/src/main/java/example/learning/kotlin/gabriel/zendone/fragments/OrganizationFragment.kt
@@ -0,0 +1,25 @@
+package example.learning.kotlin.gabriel.zendone.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import example.learning.kotlin.gabriel.zendone.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OrganizationFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_organization, container, false)
+    }
+
+}// Required empty public constructor
